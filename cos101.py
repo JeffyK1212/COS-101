47e3aa0085d1ed4bf60b69d8ba526dc885b24ea7
JeffyK1212 <emmanuelkassa124@gmail.com>
2024-12-04 05:07:23 +0100

    Created a function a to calculate force

--- cos101.py
+++ cos101.py
@@ -5,5 +5,12 @@ print("C: Speed")
 print("D: Power")
 print("E: Voltage")
 # Ask for user input
-inputMsg = input("Choose a physics operation between A-E:").lower()
+inputMsg = input("Choose a physics operation between A-E:\n").lower()
+# A function to calculate force
+def a():
+    mass = float(input("What is the mass (in kg)?:\n"))
+    print("What is the acceleration (in m/s2) ?:")
+    acceleration = float(input())
+    force = mass * acceleration
+    print(f"The force is {force} Newtons.")
 
