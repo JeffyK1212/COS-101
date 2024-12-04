b8bacdda4dd8678d2d3bcd3bf9eed832353ca9fc
JeffyK1212 <emmanuelkassa124@gmail.com>
2024-12-04 05:05:36 +0100

    Created a main menu for the users to choose thier Physics operations

---
+++ cos101.py
@@ -0,0 +1,9 @@
+print("This is a program created for Physics calculations.")
+print("A: Force")
+print("B: Work Done")
+print("C: Speed")
+print("D: Power")
+print("E: Voltage")
+# Ask for user input
+inputMsg = input("Choose a physics operation between A-E:").lower()
+
