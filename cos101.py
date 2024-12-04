9ab251bdc0fc2f62eb546110b143c6e74188d97d
JeffyK1212 <emmanuelkassa124@gmail.com>
2024-12-04 05:10:43 +0100

    Created a function B to calculate work done

--- cos101.py
+++ cos101.py
@@ -9,8 +9,14 @@ inputMsg = input("Choose a physics operation between A-E:\n").lower()
 # A function to calculate force
 def a():
     mass = float(input("What is the mass (in kg)?:\n"))
-    print("What is the acceleration (in m/s2) ?:")
-    acceleration = float(input())
+    acceleration = float(input("What is the acceleration (in m/s2) ?:"))
     force = mass * acceleration
     print(f"The force is {force} Newtons.")
+# A function to calculate work done
+def b():
+    force = float(input("What is the force (in Newtons)?:\n"))
+    distance = float(input("What is the distance (in metres)?:\n"))
+    workDone = force * distance
+    print(f"The workdone is {workDone} Joules.")
+
 
